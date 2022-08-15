b05900a5e81904300766c8902b0a4cce23227816
Allison Bohannon <akboh.github@gmail.com>
2022-08-14 21:40:22 -0700

    commit tree d5a748f6e7f1c3555a0cb3cbe95833fb8c312b74

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const displayMovies = movies.map(movie => {
+    return (
+      <div key={movie.title}>
+        Title: {movie.title}
+        <br></br>
+        Runtime: {movie.time}
+        <ul>
+          {movie.genres.map(genre => {return <li key={genre}>{genre}</li>})}
+        </ul>
+      </div>
+    )
+  }
+  )
+
+  return ( 
+  <div>
+    <h1>Movies Page</h1>
+    {displayMovies}
+  </div>
+  );
 }
 
 export default Movies;
